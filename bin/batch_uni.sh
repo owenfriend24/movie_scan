38886a3db8671b5ba41a4c9d2ce8ef9885c9a857
owenfriend24 <owenwfriend@gmail.com>
2025-07-02 19:21:46 -0500

    movie processing functions

--- bin/batch_uni.sh
+++ bin/batch_uni.sh
@@ -24,8 +24,11 @@ source $HOME/analysis/temple/profile
 ppl_2="/home1/09123/ofriend/analysis/movie_scan/perplexity_2nd_level.fsf"
 bayes_2="/home1/09123/ofriend/analysis/movie_scan/bayes_2nd_level.fsf"
 
-python /home1/09123/ofriend/analysis/movie_scan/bin/edit_first_uni.py ${ppl_2} $out_path $subject 5 2222 $num_vox1 "movie"
-python /home1/09123/ofriend/analysis/movie_scan/bin/edit_first_uni.py ${bayes_2} $out_path $subject 5 2222 $num_vox2 "movie"
+out_path=${fmriprep_dir}/sub-${sub}/
+subject=$sub
+
+python /home1/09123/ofriend/analysis/movie_scan/bin/edit_first_uni.py ${ppl_2} $out_path $subject 5 2222 999 "movie"
+python /home1/09123/ofriend/analysis/movie_scan/bin/edit_first_uni.py ${bayes_2} $out_path $subject 5 2222 999 "movie"
 
 feat /scratch/09123/ofriend/movie_scan/sub-${sub}/sub-${sub}-uni_ppl_second_level.fsf
 feat /scratch/09123/ofriend/movie_scan/sub-${sub}/sub-${sub}-uni_bayes_second_level.fsf
