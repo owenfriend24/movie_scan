ebb4f3e7433e028e687f605ea1535568d55c1214
owenfriend24 <owenwfriend@gmail.com>
2025-07-06 14:57:02 -0500

    movie processing functions

--- bin/mv_clust_sim.sh
+++ bin/mv_clust_sim.sh
@@ -15,8 +15,8 @@ export OMP_NUM_THREADS=None
 mkdir -p ${fmriprep_dir}/clust_sim
 cd ${fmriprep_dir}/clust_sim
 
-3dClustSim -mask /home1/09123/ofriend/analysis/movie_scan/b_hip_func.nii.gz -acf 0.620 2.485 7.502 -nodec -prefix bayes_hip
-3dClustSim -mask /home1/09123/ofriend/analysis/movie_scan/mni_gm_2mm_dil_masked.nii.gz -acf 0.620 2.485 7.502 -nodec -prefix bayes_gm
+#3dClustSim -mask /home1/09123/ofriend/analysis/movie_scan/b_hip_func.nii.gz -acf 0.620 2.485 7.502 -nodec -prefix bayes_hip
+3dClustSim -mask /scratch/09123/ofriend/movie_scan/mni_masks/group_75_mask.nii.gz -acf 0.620 2.485 7.502 -nodec -prefix bayes_gm_new
 
-3dClustSim -mask /home1/09123/ofriend/analysis/movie_scan/b_hip_func.nii.gz -acf 0.620 2.485 7.495 -nodec -prefix ppl_hip
-3dClustSim -mask /home1/09123/ofriend/analysis/movie_scan/mni_gm_2mm_dil_masked.nii.gz -acf 0.620 2.485 7.495 -nodec -prefix ppl_gm
+#3dClustSim -mask /home1/09123/ofriend/analysis/movie_scan/b_hip_func.nii.gz -acf 0.620 2.485 7.495 -nodec -prefix ppl_hip
+3dClustSim -mask /scratch/09123/ofriend/movie_scan/mni_masks/group_75_mask.nii.gz -acf 0.620 2.485 7.495 -nodec -prefix ppl_gm_new
