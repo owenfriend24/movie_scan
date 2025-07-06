ba213c394c7832c5f1e457ed35682a2d43d3a1e7
owenfriend24 <owenwfriend@gmail.com>
2025-07-06 14:35:48 -0500

    movie processing functions

--- bin/acf.sh
+++ bin/acf.sh
@@ -10,7 +10,7 @@ subject="$2"
 module load afni
 
 out_file="${fmriprep_dir}/bayes_residuals/by_subject_acf.txt"
-mask_path="/corral-repl/utexas/prestonlab/temple/freesurfer/sub-${subject}/mri/out/brainmask_func_movie_dilated.nii.gz"
+mask_path="/corral-repl/utexas/prestonlab/temple/freesurfer/sub-${subject}/mri/b_gray_movie.nii.gz"
 
 for run in {1..2}; do
     resid_path="${fmriprep_dir}/bayes_residuals/sub-${subject}_bayes_r${run}_resid.nii.gz"
@@ -25,7 +25,7 @@ for run in {1..2}; do
 done
 
 out_file="${fmriprep_dir}/ppl_residuals/by_subject_acf.txt"
-mask_path="/corral-repl/utexas/prestonlab/temple/freesurfer/sub-${subject}/mri/out/brainmask_func_movie_dilated.nii.gz"
+mask_path="/corral-repl/utexas/prestonlab/temple/freesurfer/sub-${subject}/mri/out/b_gray_movie.nii.gz"
 
 for run in {1..2}; do
     resid_path="${fmriprep_dir}/ppl_residuals/sub-${subject}_ppl_r${run}_resid.nii.gz"
