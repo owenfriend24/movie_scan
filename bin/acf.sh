8de49733823fbb71e53368496d8aa41b56d17d45
owenfriend24 <owenwfriend@gmail.com>
2025-07-06 12:42:26 -0500

    movie processing functions

---
+++ bin/acf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+    echo "Usage: temple_acf.sh fmriprep_dir subject"
+    exit 1
+fi
+
+fmriprep_dir="$1"
+subject="$2"
+module load afni
+
+out_file="${fmriprep_dir}/bayes_acf/by_subject_acf.txt"
+mask_path="/corral-repl/utexas/prestonlab/temple/freesurfer/sub-${subject}/mri/out/brainmask_func_movie_dilated.nii.gz"
+
+for run in {1..2}; do
+    resid_path="${fmriprep_dir}/bayes_residuals/${subject}_bayes_r${run}_resid.nii.gz"
+
+    if [[ -f "$resid_path" ]]; then
+        output=$(3dFWHMx -mask "$mask_path" -ACF NULL -input "$resid_path" -arith)
+        acf_coefs=$(echo "$output" | tail -n 1 | awk '{print $(NF-3), $(NF-2), $(NF-1), $NF}')
+        echo "$subject $run $acf_coefs" >> "$out_file"
+    else
+        echo "Missing residual file for $subject run $run"
+    fi
+done
+
+out_file="${fmriprep_dir}/ppl_acf/by_subject_acf.txt"
+mask_path="/corral-repl/utexas/prestonlab/temple/freesurfer/sub-${subject}/mri/out/brainmask_func_movie_dilated.nii.gz"
+
+for run in {1..2}; do
+    resid_path="${fmriprep_dir}/ppl_residuals/${subject}_ppl_r${run}_resid.nii.gz"
+
+    if [[ -f "$resid_path" ]]; then
+        output=$(3dFWHMx -mask "$mask_path" -ACF NULL -input "$resid_path" -arith)
+        acf_coefs=$(echo "$output" | tail -n 1 | awk '{print $(NF-3), $(NF-2), $(NF-1), $NF}')
+        echo "$subject $run $acf_coefs" >> "$out_file"
+    else
+        echo "Missing residual file for $subject run $run"
+    fi
+done
+
