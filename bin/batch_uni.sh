8288086021bf849d83378b1603e8a9ac5928acec
owenfriend24 <owenwfriend@gmail.com>
2025-07-25 11:00:44 -0500

    movie processing functions

--- bin/batch_uni.sh
+++ bin/batch_uni.sh
@@ -15,27 +15,27 @@ corr=/corral-repl/utexas/prestonlab/temple
 source $HOME/analysis/temple/profile
 #
 
-/home1/09123/ofriend/analysis/movie_scan/bin/run_first_movie_models.sh \
-${fmriprep_dir} ${sub} /corral-repl/utexas/prestonlab/temple/ parse
+#/home1/09123/ofriend/analysis/movie_scan/bin/run_first_movie_models.sh \
+#${fmriprep_dir} ${sub} /corral-repl/utexas/prestonlab/temple/ parse
 
-#/home1/09123/ofriend/analysis/movie_scan/bin/run_first_movie_models.sh \
-#${fmriprep_dir} ${sub} /corral-repl/utexas/prestonlab/temple/ ppl
-#
-#/home1/09123/ofriend/analysis/movie_scan/bin/run_first_movie_models.sh \
-#${fmriprep_dir} ${sub} /corral-repl/utexas/prestonlab/temple/ bayes
+/home1/09123/ofriend/analysis/movie_scan/bin/run_first_movie_models.sh \
+${fmriprep_dir} ${sub} /corral-repl/utexas/prestonlab/temple/ ppl
+
+/home1/09123/ofriend/analysis/movie_scan/bin/run_first_movie_models.sh \
+${fmriprep_dir} ${sub} /corral-repl/utexas/prestonlab/temple/ bayes
 
 
 #ppl_2="/home1/09123/ofriend/analysis/movie_scan/perplexity_2nd_level.fsf"
 #bayes_2="/home1/09123/ofriend/analysis/movie_scan/bayes_2nd_level.fsf"
-
+#
 #out_path=${fmriprep_dir}/sub-${sub}/
 #subject=$sub
-
+#
 #python /home1/09123/ofriend/analysis/movie_scan/bin/edit_first_uni.py ${ppl_2} $out_path $subject 5 2222 999 "movie"
 #python /home1/09123/ofriend/analysis/movie_scan/bin/edit_first_uni.py ${bayes_2} $out_path $subject 5 2222 999 "movie"
-
+#
 #feat /scratch/09123/ofriend/movie_scan/sub-${sub}/sub-${sub}-uni_ppl_second_level.fsf
 #feat /scratch/09123/ofriend/movie_scan/sub-${sub}/sub-${sub}-uni_bayes_second_level.fsf
 
-
-feat /scratch/09123/ofriend/movie_scan/sub-${sub}/sub-${sub}-uni_parse_second_level.fsf
+#
+#feat /scratch/09123/ofriend/movie_scan/sub-${sub}/sub-${sub}-uni_parse_second_level.fsf
