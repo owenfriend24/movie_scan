efb7a6b8ac8fdf2a0381583812ad08fd7e449ce5
owenfriend24 <owenwfriend@gmail.com>
2025-08-03 10:27:33 -0500

    movie processing functions

--- bin/run_first_movie_models.sh
+++ bin/run_first_movie_models.sh
@@ -15,15 +15,15 @@ measure=$4
 
 for run in 1 2; do
     echo "running first level analysis for sub ${subject}..."
-    feat "${fmriprep_dir}/sub-${subject}/sub-${subject}-uni_level1_${measure}_run-0${run}.fsf"
-    chmod 775 -R "${corral}/sub-${subject}/transforms/movie"
+#    feat "${fmriprep_dir}/sub-${subject}/sub-${subject}-uni_level1_${measure}_run-0${run}.fsf"
+#    chmod 775 -R "${corral}/sub-${subject}/transforms/movie"
 
-    echo "saving first level output to native directory"
-    mkdir -p "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native"
-    cp -r "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/stats/"* "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native"
-    cp "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/example_func.nii.gz" "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native/example_func.nii.gz"
-    cp "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/mean_func.nii.gz" "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native/mean_func.nii.gz"
-    cp "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/mask.nii.gz" "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native/mask.nii.gz"
+#    echo "saving first level output to native directory"
+#    mkdir -p "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native"
+#    cp -r "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/stats/"* "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native"
+#    cp "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/example_func.nii.gz" "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native/example_func.nii.gz"
+#    cp "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/mean_func.nii.gz" "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native/mean_func.nii.gz"
+#    cp "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/mask.nii.gz" "${fmriprep_dir}/sub-${subject}/${measure}_out_run${run}.feat/native/mask.nii.gz"
 
 
     # cope images
